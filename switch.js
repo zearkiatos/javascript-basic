89e4ad5745b3cac379c82306a6675a2b115f10d5
zearkiatos <caprilespe@outlook.com>
2021-08-10 00:09:29 -0400

    Switch

---
+++ switch.js
@@ -0,0 +1,14 @@
+var number = 30;
+switch(number) {
+    case 1:
+        console.log("I'm one");
+        break;
+    case 10:
+        console.log("I'm an ten");
+        break;
+    case 100:
+        console.log("I'm an one hundred");
+        break;
+    default:
+        console.log("I'm anything");
+}
---
+++ test/rockPaperOrScissorsWithSwitch.unit.test.js
@@ -0,0 +1,76 @@
+import rockPaperOrScissorsSwitch from '../src/rockPaperOrScissorsWithSwitch';
+
+describe('Test suite of game rock, paper or scissors 🪨 📄  ✂️ ', () => {
+
+    test('Should show a tie if player1 and player2 send de same option', () => {
+        const player1Option = 'rock';
+        const player2Option = 'rock';
+
+        const result = rockPaperOrScissorsSwitch(player1Option, player2Option);
+
+        expect(result).toBe('Player1 and Player2 are tie')
+    })
+    test('Should send player1 option with rock and player2 scissors and the winner should be player 1', () => {
+        const player1Option = 'rock';
+        const player2Option = 'scissors';
+
+        const result = rockPaperOrScissorsSwitch(player1Option, player2Option);
+
+        expect(result).toBe('The winner is player 1 with rock option')
+    })
+
+    test('Should send player1 option with paper and player2 scissors and the winner should be player 2', () => {
+        const player1Option = 'paper';
+        const player2Option = 'scissors';
+
+        const result = rockPaperOrScissorsSwitch(player1Option, player2Option);
+
+        expect(result).toBe('The winner is player 2 with scissors option')
+    })
+
+    test('Should send player1 option with scissors and player2 rock and the winner should be player 2', () => {
+        const player1Option = 'scissors';
+        const player2Option = 'rock';
+
+        const result = rockPaperOrScissorsSwitch(player1Option, player2Option);
+
+        expect(result).toBe('The winner is player 2 with rock option')
+    })
+
+    test('Should send player1 option with scissors and player2 paper and the winner should be player 1', () => {
+        const player1Option = 'scissors';
+        const player2Option = 'paper';
+
+        const result = rockPaperOrScissorsSwitch(player1Option, player2Option);
+
+        expect(result).toBe('The winner is player 1 with scissors option')
+    })
+
+    test('Should send player1 option with paper and player2 scissors and the winner should be player 2', () => {
+        const player1Option = 'paper';
+        const player2Option = 'scissors';
+
+        const result = rockPaperOrScissorsSwitch(player1Option, player2Option);
+
+        expect(result).toBe('The winner is player 2 with scissors option')
+    })
+
+    test('Should send player1 option with paper and player2 rock and the winner should be player 1', () => {
+        const player1Option = 'paper';
+        const player2Option = 'rock';
+
+        const result = rockPaperOrScissorsSwitch(player1Option, player2Option);
+
+        expect(result).toBe('The winner is player 1 with paper option')
+    })
+
+    test('Should send player1 option with rock and player2 paper and the winner should be player 2', () => {
+        const player1Option = 'rock';
+        const player2Option = 'paper';
+
+        const result = rockPaperOrScissorsSwitch(player1Option, player2Option);
+
+        expect(result).toBe('The winner is player 2 with paper option')
+    })
+
+})
