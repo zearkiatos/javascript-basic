7f72515867730fd3695c79fc01e23e4944fd0d22
zearkiatos <caprilespe@outlook.com>
2021-08-10 21:03:31 -0400

    Arrays

---
+++ arrays.js
@@ -0,0 +1,27 @@
+var fruits = ["Apple", "Banana", "Cherry", "Strawberry"];
+
+console.log(fruits);
+
+console.log(fruits.length);
+
+console.log(fruits[0]);
+
+console.log(fruits[2]);
+
+var moreFruits = fruits.push("Grapes");
+
+console.log(fruits);
+
+var last = fruits.pop();
+
+console.log(fruits);
+
+var newLongitud = fruits.unshift("Grapes");
+
+var deleteFruit = fruits.shift("Grapes");
+
+console.log(fruits);
+
+var position = fruits.indexOf("Cherry");
+
+console.log(position);
